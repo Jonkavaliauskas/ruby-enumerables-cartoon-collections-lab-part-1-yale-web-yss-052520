2788aa20d6fb2d1fbfcbf6b0b6849094cfc22c85
Jonas Kavaliauskas <jonas.kavaliauskas@yale.edu>
2020-05-21 14:40:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,6 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |index, name|
-    puts index + "." + name
+    puts index.to_s + "." + name
   end
 end
